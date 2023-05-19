0e4648c5f5f97dee539e49fd6d92f768da4eb62c
orikage <63395401+orikage@users.noreply.github.com>
2023-05-19 13:20:18 +0900

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,23 @@
+import matplotlib
+import math
+import matplotlib.pyplot as plt
+
+
+path="./logdata.txt"
+f=open(path)
+input_data=f.read()
+gruoping=3
+
+splited_data=input_data.split()
+#print(splited_data)
+
+j=0
+for i in splited_data:
+
+    theta=((2*math.pi)/(1080/gruoping))*j+(math.pi/2)
+    x=float(i)*math.cos(theta)
+    y=float(i)*math.sin(theta)
+    #print(f"x,y={x,y}")
+    plt.plot([0,x],[0,y])
+    j+=1
+plt.show()
